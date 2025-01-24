da583223a273d502ca032585b6774491630c2581
alan <rubikscubesrs@gmail.com>
2025-01-24 07:49:51 -0800

    added some birthdays

--- functions/birthdays.js
+++ functions/birthdays.js
@@ -5,7 +5,7 @@ let a = [
     {name: `Kate's birthday`, timezone: `Europe/London`, birthday: "01/20/2000"}
     , {name: `Orefist's birthday`, timezone: `Asia/Singapore`, birthday: "01/11"}
     , {name: `Rebecca's birthday`, timezone: `America/Chicago`, birthday: "02/01/1995"}
-    , {name: `Gumbo's birthday`, timezone: `America/Chicago`, birthday: "01/30"}
+    , {name: `Gumbo's birthday`, timezone: `America/Chicago`, birthday: "01/30/1992"}
     , {name: `bun's birthday`, timezone: `America/Los_Angeles`, birthday: "12/16/1980"}
     , {name: `Kayla's birthday`, timezone: `America/New_York`, birthday: "12/07/1994"}
     , {name: `Jen's birthday`, timezone: `America/New_York`, birthday: "06/08/1996"}
@@ -15,6 +15,8 @@ let a = [
     , {name: `Sharon's birthday`, timezone: `America/Los_Angeles`, birthday: "10/06/1986"}
     , {name: `Jasmin's birthday`, timezone: `America/Los_Angeles`, birthday: "01/14/1985"}
     , {name: `Lori's birthday`, timezone: `America/Los_Angeles`, birthday: "12/22/1982"}
+    , {name: `Jacob's birthday`, timezone: `America/Los_Angeles`, birthday: "1/16"}
+    , {name: `Dib's birthday`, timezone: `America/New_York`, birthday: "10/23"}
 ];
 
 export function loadBirthdays(client) {
