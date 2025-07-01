d354a3d1d2f68b5e9d1ee10d681d2ec0688f3e47
alan <rubikscubesrs@gmail.com>
2025-07-01 16:20:24 -0700

    added ryan pings

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,7 @@
 import wildyEvents from '../commands/wildyevents.js';
 import clues from '../commands/clues.js';
 import { loadBirthdays } from '../functions/birthdays.js';
+import { Cron } from "croner";
 
 export default {
 	name: 'ready',
@@ -12,5 +13,14 @@ export default {
 		await wildyEvents.hourlyNotification(client);
 		await clues.fetchPrices();
 		loadBirthdays(client);
+        client.users.fetch('202233700572725249')
+		.then(user => {
+			const cron = new Cron(`0 0 * * * *`, {timezone: value.timezone}, () => {
+				user.send({
+					content: `# RYAN USE HOUSE TELEPORT FOR KAGS AND MENAPHOS SCAN`
+				});
+			});
+			console.log(cron.nextRuns(3));
+		}).catch(err => console.log(err));
 	},
 };
