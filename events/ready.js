b9a57d3c9a3f812837468b9742368acd1d40c093
alan <rubikscubesrs@gmail.com>
2025-07-08 11:31:07 -0700

    removed onload stuffs

--- events/ready.js
+++ events/ready.js
@@ -10,17 +10,17 @@ export default {
 	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 		// await bingo.prePopulateValues();
-		await wildyEvents.hourlyNotification(client);
-		await clues.fetchPrices();
-		loadBirthdays(client);
-        client.users.fetch('202233700572725249')
-		.then(user => {
-			const cron = new Cron(`0 0 * * * *`, {timezone: value.timezone}, () => {
-				user.send({
-					content: `# RYAN USE HOUSE TELEPORT FOR KAGS AND MENAPHOS SCAN`
-				});
-			});
-			console.log(cron.nextRuns(3));
-		}).catch(err => console.log(err));
+		// await wildyEvents.hourlyNotification(client);
+		// await clues.fetchPrices();
+		// loadBirthdays(client);
+        // client.users.fetch('202233700572725249')
+		// .then(user => {
+		// 	const cron = new Cron(`0 0 * * * *`, {timezone: value.timezone}, () => {
+		// 		user.send({
+		// 			content: `# RYAN USE HOUSE TELEPORT FOR KAGS AND MENAPHOS SCAN`
+		// 		});
+		// 	});
+		// 	console.log(cron.nextRuns(3));
+		// }).catch(err => console.log(err));
 	},
 };
